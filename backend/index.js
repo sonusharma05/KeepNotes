b5d8595b764ca91fdffe031e6953988c2fa8a54b
sonusharma05 <128077876+sonusharma05@users.noreply.github.com>
2025-12-27 00:05:39 +0530

    Refactor notes routes to implement async functionality and improve response handling

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,28 @@
-const express = require("express")
+const express = require("express");
+const { notes } = require("./db");
 const app = express();
 app.use(express.json()) ;
 app.get("/test", (req,res)=>{
     res.send("backend is running ");
-})
-app.get("/notes",(req,res)=>{
-    res.send("hello world")
-})
-app.post("/notes",(req,res)=>{
     
+})
+app.get("/notes",async(req,res)=>{
+    
+   const a = await notes.find();
+   res.send(a);
+})
+app.post("/notes",async (req,res)=>{
+    const title = req.body.title;
+    const noteBody = req.body.body;
+
+    await notes.create({
+        title:title,
+        body:noteBody
+    })
+    res.json({
+        msg:"Note Created"
+    })
+   console.log("running fine") 
 })
 
 app.put("/notes",(req,res)=>{
